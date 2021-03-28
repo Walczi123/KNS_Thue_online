f7c48d0e2ae29a7b40f94a1bc85cb8291d15b53e
Patryk <patryk.wal@onet.pl>
2021-03-28 11:37:21 +0200

    game

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ThueOnline.Game;
+
+namespace ThueOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alphabet alphabet = new Alphabet(3);
+            int limit = 7;
+            var game = new ThueGame(alphabet, limit);
+            game.Start();
+            Console.WriteLine("Bye");
+            Console.ReadKey();
+        }
+    }
+}
